0fc96181169db9309239281c548df6a472ef6a4f
bahae3 <bahaeddiine12@gmail.com>
2025-02-10 14:35:19 +0100

    Second commit

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test code");
+        System.out.println("Test code. Changed with bahzak branch.");
     }
 }
